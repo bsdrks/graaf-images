3d4858c50fd0d83d0a5afd892dbecd5a451401a5
Bas Dirks <bas.dirks@purplestorytelling.com>
2024-08-19 17:42:11 +0200

    add `gen` graphs

--- dot.sh
+++ dot.sh
@@ -1,3 +1,6 @@
-dot -Tsvg src/complete_2.dot > out/complete_2.svg
-dot -Tsvg src/complete_3.dot > out/complete_3.svg
-dot -Tsvg src/complete_4.dot > out/complete_4.svg
+FILES=$(ls src/*.dot)
+
+for f in $FILES
+do
+  dot -Tsvg $f > out/$(basename $f .dot).svg
+done
